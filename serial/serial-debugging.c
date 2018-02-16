1bb32b0a2738c598124ad9aa5ec6f0465a18e9e5
Tom <tomj.pinnock@outlook.com>
2018-02-16 13:18:56 +0000

    Fixed serial starting move to long mode.

--- serial/serial-debugging.c
+++ serial/serial-debugging.c
@@ -1,11 +1,23 @@
 #include "serial-debugging.h"
+#include "serial.h"
+#include <stddef.h>
+
+#include "display/vga.h"
 
 void init_debug_port()
 {
-	outb(
+	init_serial_port(DEBUG_PORT);
 }
 
 void debug_print(const char* str)
 {
-	
+	term_print("Debug Print: ");
+	for (size_t i = 0; str[i] != '\0'; i++)
+	{
+		term_putc(str[i]);
+		write_serial(DEBUG_PORT, str[i]);
+	}
+	write_serial(DEBUG_PORT, '\r');
+	write_serial(DEBUG_PORT, '\n');
+	term_putc('\n');
 }
--- start.s
+++ start.s
@@ -1,41 +1,109 @@
-//Declare kernal main as external function for linking
 .extern kernel_main
+/* Declare constants for the multiboot header. */
+.set ALIGN,    1<<0             /* align loaded modules on page boundaries */
+.set MEMINFO,  1<<1             /* provide memory map */
+.set FLAGS,    ALIGN | MEMINFO  /* this is the Multiboot 'flag' field */
+.set MAGIC,    0x1BADB002       /* 'magic number' lets bootloader find the header */
+.set CHECKSUM, -(MAGIC + FLAGS) /* checksum of above, to prove we are multiboot */
 
-//Set start to global for linking
-.global start
-
-
-//Multiboot grub bullshit.
-// Our bootloader, GRUB, needs to know some basic information about our kernel before it can boot it.
-// We give GRUB this information using a standard known as 'Multiboot'.
-// To define a valid 'Multiboot header' that will be recognised by GRUB, we need to hard code some
-// constants into the executable. The following code calculates those constants.
-.set MB_MAGIC, 0x1BADB002          // This is a 'magic' constant that GRUB will use to detect our kernels location.
-.set MB_FLAGS, (1 << 0) | (1 << 1) // This tells GRUB to 1: load modules on page boundaries and 2: provide a memory map (this is useful later in development)
-// Finally, we calculate a checksum that includes all the previous values
-.set MB_CHECKSUM, (0 - (MB_MAGIC + MB_FLAGS))
-
-
-//Multiboot section 
+/* 
+Declare a multiboot header that marks the program as a kernel. These are magic
+values that are documented in the multiboot standard. The bootloader will
+search for this signature in the first 8 KiB of the kernel file, aligned at a
+32-bit boundary. The signature is in its own section so the header can be
+forced to be within the first 8 KiB of the kernel file.
+*/
 .section .multiboot
-	.align 4
-	.long MB_MAGIC
-	.long MB_FLAGS
-	.long MB_CHECKSUM
-
-//This section is zeroed out when the kernel starts.
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+/*
+The multiboot standard does not define the value of the stack pointer register
+(esp) and it is up to the kernel to provide a stack. This allocates room for a
+small stack by creating a symbol at the bottom of it, then allocating 16384
+bytes for it, and finally creating a symbol at the top. The stack grows
+downwards on x86. The stack is in its own section so it can be marked nobits,
+which means the kernel file is smaller because it does not contain an
+uninitialized stack. The stack on x86 must be 16-byte aligned according to the
+System V ABI standard and de-facto extensions. The compiler will assume the
+stack is properly aligned and failure to align the stack will result in
+undefined behavior.
+*/
 .section .bss
-	.align 16
-	stack_bottom:
-		.skip 4096 //We allocate 4k of ram for the stack.
-		stack_top:
+.align 16
+stack_bottom:
+.skip 16384 # 16 KiB
+stack_top:
 
+/*
+The linker script specifies _start as the entry point to the kernel and the
+bootloader will jump to this position once the kernel has been loaded. It
+doesn't make sense to return from this function as the bootloader is gone.
+*/
 .section .text
-	start:
-		mov $stack_top, %esp //stack grows down so move stack pointer to the top.
-		call kernel_main // call c code
-		//if it errors clear interrupts then halt if that doesnt work try again.
-		hang:
-			cli
-			hlt
-			jmp hang
+.global _start
+.type _start, @function
+_start:
+	/*
+	The bootloader has loaded us into 32-bit protected mode on a x86
+	machine. Interrupts are disabled. Paging is disabled. The processor
+	state is as defined in the multiboot standard. The kernel has full
+	control of the CPU. The kernel can only make use of hardware features
+	and any code it provides as part of itself. There's no printf
+	function, unless the kernel provides its own <stdio.h> header and a
+	printf implementation. There are no security restrictions, no
+	safeguards, no debugging mechanisms, only what the kernel provides
+	itself. It has absolute and complete power over the
+	machine.
+	*/
+
+	/*
+	To set up a stack, we set the esp register to point to the top of our
+	stack (as it grows downwards on x86 systems). This is necessarily done
+	in assembly as languages such as C cannot function without a stack.
+	*/
+	mov $stack_top, %esp
+
+	/*
+	This is a good place to initialize crucial processor state before the
+	high-level kernel is entered. It's best to minimize the early
+	environment where crucial features are offline. Note that the
+	processor is not fully initialized yet: Features such as floating
+	point instructions and instruction set extensions are not initialized
+	yet. The GDT should be loaded here. Paging should be enabled here.
+	C++ features such as global constructors and exceptions will require
+	runtime support to work as well.
+	*/
+
+	/*
+	Enter the high-level kernel. The ABI requires the stack is 16-byte
+	aligned at the time of the call instruction (which afterwards pushes
+	the return pointer of size 4 bytes). The stack was originally 16-byte
+	aligned above and we've since pushed a multiple of 16 bytes to the
+	stack since (pushed 0 bytes so far) and the alignment is thus
+	preserved and the call is well defined.
+	*/
+	call kernel_main
+
+	/*
+	If the system has nothing more to do, put the computer into an
+	infinite loop. To do that:
+	1) Disable interrupts with cli (clear interrupt enable in eflags).
+	   They are already disabled by the bootloader, so this is not needed.
+	   Mind that you might later enable interrupts and return from
+	   kernel_main (which is sort of nonsensical to do).
+	2) Wait for the next interrupt to arrive with hlt (halt instruction).
+	   Since they are disabled, this will lock up the computer.
+	3) Jump to the hlt instruction if it ever wakes up due to a
+	   non-maskable interrupt occurring or due to system management mode.
+	*/
+	cli
+1:	hlt
+	jmp 1b
+
+/*
+Set the size of the _start symbol to the current location '.' minus its start.
+This is useful when debugging or when you implement call tracing.
+*/
+.size _start, . - _start

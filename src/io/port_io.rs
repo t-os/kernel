1484a7bb77fa8e4e27e6b3abcd9d0df3e7a6cd56
Tom Pinnock <tomj.pinnock@outlook.com>
2018-03-06 10:56:15 +0000

    Add port struct

--- src/io/port_io.rs
+++ src/io/port_io.rs
@@ -1,7 +1,53 @@
 #![allow(dead_code)]
 
-// Deadly port_io 
+pub trait InOut {
+	unsafe fn port_in(port: u16) -> Self;
+	unsafe fn port_out(port: u16, value: Self);
+}
 
+struct Port<T: InOut> {
+	port: u16,
+	phantom: PhantomData<T>,
+	
+}
+
+impl InOut for u8 {
+	unsafe fn port_in(port: u16) -> u8 { return inb(port); } 
+	unsafe fn port_out(port: u16, value: u8) { outb(port, value); }
+}
+
+impl InOut for u16 {
+	unsafe fn port_in(port: u16) -> u16 { return inw(port); } 
+	unsafe fn port_out(port: u16, value: u16) { outw(port, value); }	
+}
+
+impl InOut for u32 {
+	unsafe fn port_in(port: u16) -> u32 { return inl(port); } 
+	unsafe fn port_out(port: u16, value: u32) { outl(port, value); }	
+}
+
+impl <T: InOut> Port<T> {
+	pub const unsafe fn new(port: u16) -> Port<T> {
+		return Port { port: port, phantom: PhantomData };
+	}
+	
+	pub fn read(&self) -> T {
+		unsafe { return T::port_in(self.port); }	
+	}
+	
+	pub fn write(&self, value: T) {
+		unsafe { T::port_out(self.port, value); }
+	}
+}
+
+
+
+
+
+
+
+
+// Deadly port_io 
 pub unsafe fn outb(port: u16, val: u8)
 {
 	asm!("outb $0, $1" : : "{al}"(val), "{dx}N"(port));
